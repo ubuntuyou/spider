13adf2b7a9b1c70da1093059f71a3b8aa3fa5fe7
ubuntuyou <43767473+ubuntuyou@users.noreply.github.com>
2020-11-06 22:21:12 -0600

    Set 8k CHR to 1

--- SpiderScroller.asm
+++ SpiderScroller.asm
@@ -4,7 +4,7 @@
 
     .db  "NES", $1a     ;identification of the iNES header
     .db  PRG_COUNT      ;number of 16KB PRG-ROM pages
-    .db  $02            ;number of 8KB CHR-ROM pages
+    .db  $01            ;number of 8KB CHR-ROM pages
     .db  $00|MIRRORING  ;mapper 0 and mirroring
     .dsb $09, $00       ;clear the remaining bytes
 
